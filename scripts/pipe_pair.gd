f8739e8a89f248a8571634e447df159be4368b2e
Bjørn-Otto Hansen <bjorn.ohansen@gmail.com>
2025-01-31 19:16:55 +0100

    Added score system

--- scripts/pipe_pair.gd
+++ scripts/pipe_pair.gd
@@ -7,15 +7,22 @@ class_name PipePair
 
 @onready var top_pipe = $TopPipe
 @onready var bottom_pipe = $BottomPipe
+@onready var goal = %Goal
 
 func _ready():
+	goal.body_entered.connect(_on_body_entered)
+	
 	var opening := randf_range(min_opening, max_opening)
 	top_pipe.position.y -= opening/2
 	bottom_pipe.position.y += opening/2
-	
 	position.y = randi_range(opening, 800)
 
 func _physics_process(delta):
 	position.x -= speed * delta
 	if position.x < -64:
 		queue_free()
+
+func _on_body_entered(body: Node2D):
+	print(body)
+	if body is Bird:
+		Signals.goal_reached.emit()

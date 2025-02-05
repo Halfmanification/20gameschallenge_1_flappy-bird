1229c59a303c73ae1344aac1348f9dde1aaface8
Bjørn-Otto Hansen <bjorn.ohansen@gmail.com>
2025-02-05 19:17:49 +0100

    Added saving of game to file

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -1,12 +1,14 @@
 extends Control
 
 @onready var score_value_label = %ScoreValueLabel
+@onready var high_score_label = %HighScoreLabel
 @onready var main_menu_button = %MainMenuButton
 
 const LEVEL_1 = preload("res://scenes/level_1.tscn")
 
 func _ready():
-	score_value_label.text = str(GameState.player_score)
+	score_value_label.text = str(GameState.player_data.current_score)
+	high_score_label.text = str(GameState.player_data.high_score)
 	main_menu_button.pressed.connect(on_main_menu_button_pressed)
 
 func on_main_menu_button_pressed():
--- scripts/game_state.gd
+++ scripts/game_state.gd
@@ -1,21 +1,34 @@
 extends Node
 
-var player_score : int = 0
+var player_data : PlayerData
 
 const LEVEL_1 = preload("res://scenes/level_1.tscn")
 
 func _ready():
 	Signals.goal_reached.connect(_on_goal_reached)
 	Signals.bird_crashed.connect(_on_bird_crashed)
+	player_data = PlayerData.new()
+	player_data.load()
 
 func _on_goal_reached():
-	player_score += 1
-	Signals.score_updated.emit(player_score)
+	player_data.current_score += 1
+	if player_data.current_score > player_data.high_score:
+		player_data.high_score = player_data.current_score
+	Signals.score_updated.emit(player_data.current_score)
 
 func _on_bird_crashed():
+	save_game()
 	get_tree().paused
 
 func reset():
-	player_score = 0
+	player_data.current_score = 0
+	load_game()
 	get_tree().change_scene_to_packed(LEVEL_1)
 	get_tree().paused = false
+
+func save_game():
+	player_data.save()
+
+func load_game():
+	player_data = PlayerData.new()
+	player_data.load()
